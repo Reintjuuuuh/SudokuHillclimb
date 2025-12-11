bf61f8cffaa77efadbda544d5903aed093b4b1b5
Edu Woldhuis <woldhuis.edu@gmail.com>
2025-12-11 17:28:54 +0100

    added optimized algorithm

--- SudokuSolver/SudokuSolver.cs
+++ SudokuSolver/SudokuSolver.cs
@@ -15,7 +15,8 @@ using System.Linq;
 
 class SudokuSolver
 {
-    static List<(Sudoku sudoku, (int row, int col) swap1, (int row, int col) swap2)> Solve(string input, int walkSize = 3, int walkTrigger = 20)
+    static List<(Sudoku sudoku, (int row, int col) swap1, (int row, int col) swap2)> Solve(string input, int walkSize = 3, int walkTrigger = 4500, bool optimizedWalk = false)
+        // For our optimized walk, the walkTrigger needs to be sufficiently high to not keep resetting the optimizedWalkSize.
     {
         Sudoku sudoku = new Sudoku(input);
 
@@ -24,6 +25,9 @@ class SudokuSolver
         var solveSteps = new List<(Sudoku sudoku, (int row, int col) swap1, (int row, int col) swap2)>();
 
         int movesSinceLastImprovement = 0;
+        int plateauLevel = 0;
+        int optimizedWalkSize = walkSize;
+
         while (sudoku.score != 0)
         {
             (Sudoku tempsudoku, var swapped1, var swapped2) = Algorithm.Iteration(sudoku);
@@ -41,20 +45,35 @@ class SudokuSolver
             }
             else if (movesSinceLastImprovement > walkTrigger)
             {
-
-                // DEBUG
-                if (totalMoves > 50000)
+                List<(Sudoku, (int row, int col), (int row, int col))> boardList;
+                if (optimizedWalk)
                 {
-                    Console.WriteLine($"{walkSize}, {totalMoves}, {sudoku.score}");
+                    if (sudoku.score != plateauLevel)
+                    {
+                        plateauLevel = sudoku.score;
+                        optimizedWalkSize = walkSize;
+                    }
+                    else
+                    {
+                        optimizedWalkSize++;
+                    }
+                    // debug; Console.WriteLine($"{sudoku.score}, {optimizedWalkSize}, {solveSteps.Count}");
+                    boardList = Algorithm.RandomWalk(sudoku, optimizedWalkSize);
                 }
-                // END DEBUG
-                movesSinceLastImprovement = 0;
-                var boardList = Algorithm.RandomWalk(sudoku, walkSize);
+                else
+                {
+                    boardList = Algorithm.RandomWalk(sudoku, walkSize);
+                }
+                
+
+
                 (sudoku, _, _) = boardList[^1];
                 foreach (var board in boardList)
                 {
                     solveSteps.Add(board);
                 }
+                movesSinceLastImprovement = 0;
+
             }
             else
             {
@@ -83,7 +102,6 @@ class SudokuSolver
 
     static void Main(String[] args)
     {
-        Console.WriteLine("Please input the sudoku grid: ");
         //string input = "0 0 3 0 2 0 6 0 0 9 0 0 3 0 5 0 0 1 0 0 1 8 0 6 4 0 0 0 0 8 1 0 2 9 0 0 7 0 0 0 0 0 0 0 8 0 0 6 7 0 8 2 0 0 0 0 2 6 0 9 5 0 0 8 0 0 2 0 3 0 0 9 0 0 5 0 1 0 3 0 0";
         //string input = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0";
         //string input = Console.ReadLine();
@@ -94,12 +112,13 @@ class SudokuSolver
             List <int> solveStepsCounter = new List<int>();
             var stopwatch = new Stopwatch();
             stopwatch.Start();
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 10; i++)
             {                
                 solveStepsCounter.Add(Solve(input).Count);
-                Console.WriteLine($"Average amount of moves: {solveStepsCounter.AsParallel().Sum() / solveStepsCounter.Count}. Average amount of time to solve (ms): {stopwatch.ElapsedMilliseconds / solveStepsCounter.Count}");
+                
             }
-           
+            Console.WriteLine($"Average amount of moves: {solveStepsCounter.AsParallel().Sum() / solveStepsCounter.Count}. Average amount of time to solve (ms): {stopwatch.ElapsedMilliseconds / solveStepsCounter.Count}");
+
 
 
 
