64463ea70356a85431dfbc4f3476163e421ab527
Bas <bas.lodewijk@gmail.com>
2026-01-22 15:45:24 +0100

    Can take input

--- SudokuSolver/Tester.cs
+++ SudokuSolver/Tester.cs
@@ -70,6 +70,7 @@ namespace Sudoku_Namespace
             for (int i = 0; i < inputAmount; i++)
             {
                 res[i] = File.ReadAllText(basePath + inputFiles[i]);
+                Console.WriteLine(res[i]);
             }
             return res;
         }
